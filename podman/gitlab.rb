ef64a62a8448ced2971af8cac26d56794962d667
Christoph Kappel <christoph@unexist.dev>
2024-03-27 11:46:01 +0100

    Added min config

--- podman/gitlab.rb
+++ podman/gitlab.rb
@@ -7,6 +7,44 @@ nginx['ssl_certificate'] = "/etc/gitlab/ssl/gitlab.crt"
 nginx['ssl_certificate_key'] = "/etc/gitlab/ssl/gitlab.key"
 #gitlab_rails['gitlab_shell_ssh_port'] = 10022
 
+# Constrained memory - https://docs.gitlab.com/omnibus/settings/memory_constrained_envs.html
+puma['worker_processes'] = 0
+sidekiq['max_concurrency'] = 10
+prometheus_monitoring['enable'] = false
+
+gitaly['configuration'] = {
+    concurrency: [
+      {
+        'rpc' => "/gitaly.SmartHTTPService/PostReceivePack",
+        'max_per_repo' => 3,
+      }, {
+        'rpc' => "/gitaly.SSHService/SSHUploadPack",
+        'max_per_repo' => 3,
+      },
+    ],
+    cgroups: {
+        repositories: {
+            count: 2,
+        },
+        #mountpoint: '/sys/fs/cgroup',
+        hierarchy_root: 'gitaly',
+        memory_bytes: 500000,
+        cpu_shares: 512,
+    },
+}
+
+gitaly['env'] = {
+  'GITALY_COMMAND_SPAWN_MAX_PARALLEL' => '2'
+}
+
+gitlab_rails['env'] = {
+  'MALLOC_CONF' => 'dirty_decay_ms:1000,muzzy_decay_ms:1000'
+}
+
+gitaly['env'] = {
+  'MALLOC_CONF' => 'dirty_decay_ms:1000,muzzy_decay_ms:1000'
+}
+
 ## Roles for multi-instance GitLab
 ##! The default is to have no roles enabled, which results in GitLab running as an all-in-one instance.
 ##! Options:
